2ed3f35d72ca355207c5fd9b20ed0b370d3ae35d
samgvel@gmail.com <samgvel@gmail.com>
2022-05-25 11:25:22 +0530

    Removed Strict mode to avoid rendering twice

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,19 +4,16 @@ import { ChakraProvider } from '@chakra-ui/react'
 import customTheme from "../lib/chakra/theme";
 import { Provider } from 'react-redux';
 import { store } from '../store/store';
-import { StrictMode } from 'react';
 import React from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
 
   return (
-    <React.StrictMode>
       <Provider store={store}>
         <ChakraProvider theme={customTheme}>
           <Component {...pageProps} />
         </ChakraProvider>
       </Provider>
-    </React.StrictMode>
   );
 }
 
